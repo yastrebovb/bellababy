6dc2d45a461ab43b19f16b02828a2d8f9aa15da4
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-02-14 23:21:52 +0300

    products section

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,5 @@
 import '../scss/main.scss'
 
-import Glide from '@glidejs/glide'
+import s from './product'
 
-new Glide('.glide--reviews', {
-  type: 'carousel',
-  perView: 3,
-  focusAt: 'center',
-  gap: '32px',
-  unlimited: true,
-  breakpoints: {
-    800: {
-      perView: 2
-    },
-    480: {
-      perView: 1
-    }
-  }
-}).mount()
-
-new Glide('.glide--features', {
-  type: 'carousel',
-  perView: 4,
-  focusAt: 'center',
-  gap: '56px',
-  unlimited: true,
-  breakpoints: {
-    800: {
-      perView: 2
-    },
-    480: {
-      perView: 1
-    }
-  }
-}).mount()
+s()
