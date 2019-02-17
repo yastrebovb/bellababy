de19c7c1f65328caec95a4cf2f701ad52a48e2a4
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-02-17 19:06:30 +0300

    smooth-scroll added (and polyfill added)

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
 import '../scss/main.scss'
+
 import mobileMenu from './menu'
 import initCarousels from './carousel'
 
+import smoothscroll from 'smoothscroll-polyfill'
+
 mobileMenu()
 initCarousels()
+
+smoothscroll.polyfill()
