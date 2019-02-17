e4ece40a2fcda7966b1f9694c7580ba3ec4dbf62
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-02-17 21:10:51 +0300

    form component

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,13 @@
 const mobileMenu = () => {
-  document.querySelector('.header__menu').addEventListener('click', e => {
-    document
-      .querySelector('.header__nav__content')
-      .classList.toggle('header__nav__content--visible')
-  })
+  document
+    .querySelector('.header__nav--mobile')
+    .addEventListener('click', e => {
+      document
+        .querySelector('.header__nav__content')
+        .classList.toggle('header__nav__content--visible')
+
+      console.log('sd')
+    })
 }
 
 export default mobileMenu
