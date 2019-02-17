b57084ea3f46907035f30c0593186597d68fcaa6
Boris Yastrebov <contact.yastrebov@gmail.com>
2019-02-17 19:21:17 +0300

    smooth polyfill removed

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,5 @@ import '../scss/main.scss'
 import mobileMenu from './menu'
 import initCarousels from './carousel'
 
-import smoothscroll from 'smoothscroll-polyfill'
-
 mobileMenu()
 initCarousels()
-
-smoothscroll.polyfill()
